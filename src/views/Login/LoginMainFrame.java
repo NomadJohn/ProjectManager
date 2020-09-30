a983c319c6c13c21ed77b581f7308004e9b6d4c7
chengiiii <1692485859@qq.com>
2020-09-30 14:06:17 +0800

    修改登录 注册界面

--- src/views/Login/LoginMainFrame.java
+++ src/views/Login/LoginMainFrame.java
@@ -7,35 +7,53 @@ package views.Login;
 import uitls.Utils;
 
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class LoginMainFrame extends JFrame {
+    private JPanel contentPane;
+    private JTextField tStudentName;
+    private JPasswordField tStudentPassword;
+
     public LoginMainFrame(int width, int height) {
-        this.setSize(width, height);
-        this.setLocationRelativeTo(null);
+        setTitle("登录");
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, width, height);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
 
-        JPanel panel = new JPanel();
-        Utils.addWithLabel(panel, "姓名");
-        Utils.addWithLabel(panel, "密码");
-        JPanel submitPanel = new JPanel();
-        JButton submitBtn = new JButton("登录");
-        submitPanel.add(submitBtn);
-        panel.add(submitPanel);
-        panel.setLayout(new GridLayout(3, 1));
+        JLabel lbStudentName = new JLabel("\u59D3\u540D");
+        lbStudentName.setBounds(126, 68, 45, 18);
+        contentPane.add(lbStudentName);
 
-        this.setContentPane(panel);
+        JLabel lbStudentPassword = new JLabel("\u5BC6\u7801");
+        lbStudentPassword.setBounds(126, 111, 45, 18);
+        contentPane.add(lbStudentPassword);
 
-        submitBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
+        tStudentName = new JTextField();
+        tStudentName.setBounds(170, 65, 128, 24);
+        contentPane.add(tStudentName);
+        tStudentName.setColumns(10);
+
+        JButton sumbitBtn = new JButton("\u767B\u5F55");
+        sumbitBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
 
             }
         });
+        sumbitBtn.setBounds(169, 159, 113, 27);
+        contentPane.add(sumbitBtn);
+
+        tStudentPassword = new JPasswordField();
+        tStudentPassword.setBounds(170, 108, 128, 24);
+        contentPane.add(tStudentPassword);
     }
 
     public static void main(String[] args) {
-        new LoginMainFrame(400, 300).setVisible(true);
+        new LoginMainFrame(450, 300).setVisible(true);
     }
 }
--- src/views/Login/StudentRegisterFrame.java
+++ src/views/Login/StudentRegisterFrame.java
@@ -3,41 +3,91 @@ package views.Login;
 import uitls.Utils;
 
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.NumberFormat;
 
 public class StudentRegisterFrame extends JFrame {
+    private JPanel contentPane;
+    private JTextField tStudentId;
+    private JTextField tStudentName;
+    private JTextField tStudentPassword;
+    private JTextField tStudentPasswordConfirm;
+
     public StudentRegisterFrame(int width, int height) {
-        this.setSize(width, height);
-        this.setLocationRelativeTo(null);
-        this.setTitle("注册");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("注册");
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, width, height);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
 
-        JPanel panel = new JPanel();
-        JTextField tStudentId = Utils.addWithLabel(panel, "学       号");
-        JTextField tStudentName =  Utils.addWithLabel(panel, "姓       名");
-        JTextField tStudentSex =  Utils.addWithLabel(panel, "性       别");
-        JTextField tStudentAge =  Utils.addWithLabel(panel, "年       龄");
-        JTextField tStudentPassword =  Utils.addWithLabel(panel, "密       码");
-        JTextField tStudentPasswordConfirm =  Utils.addWithLabel(panel, "确认密码");
-        JPanel submitPanel = new JPanel();
-        JButton submitBtn = new JButton("注册");
-        submitPanel.add(submitBtn);
+        JLabel lbStudentId = new JLabel("\u5B66\u53F7");
+        lbStudentId.setBounds(116, 16, 42, 18);
+        contentPane.add(lbStudentId);
 
-        panel.add(submitPanel);
-        panel.setLayout(new GridLayout(7, 1));
-        this.setContentPane(panel);
+        tStudentId = new JTextField();
+        tStudentId.setBounds(202, 13, 106, 24);
+        contentPane.add(tStudentId);
+        tStudentId.setColumns(10);
 
-        submitBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
+        JLabel lbStudentName = new JLabel("\u59D3\u540D");
+        lbStudentName.setBounds(116, 50, 42, 18);
+        contentPane.add(lbStudentName);
+
+        tStudentName = new JTextField();
+        tStudentName.setColumns(10);
+        tStudentName.setBounds(202, 47, 106, 24);
+        contentPane.add(tStudentName);
+
+        JLabel lbStudentSex = new JLabel("\u6027\u522B");
+        lbStudentSex.setBounds(116, 84, 42, 18);
+        contentPane.add(lbStudentSex);
+
+        JLabel lbStudentAge = new JLabel("\u5E74\u9F84");
+        lbStudentAge.setBounds(116, 118, 42, 18);
+        contentPane.add(lbStudentAge);
+
+        JLabel lbStudentPassword = new JLabel("\u5BC6\u7801");
+        lbStudentPassword.setBounds(116, 152, 42, 18);
+        contentPane.add(lbStudentPassword);
+
+        tStudentPassword = new JTextField();
+        tStudentPassword.setColumns(10);
+        tStudentPassword.setBounds(202, 149, 106, 24);
+        contentPane.add(tStudentPassword);
+
+        JLabel lbStudentPasswordConfirm = new JLabel("\u786E\u8BA4\u5BC6\u7801");
+        lbStudentPasswordConfirm.setBounds(116, 186, 72, 18);
+        contentPane.add(lbStudentPasswordConfirm);
+
+        tStudentPasswordConfirm = new JTextField();
+        tStudentPasswordConfirm.setColumns(10);
+        tStudentPasswordConfirm.setBounds(202, 183, 106, 24);
+        contentPane.add(tStudentPasswordConfirm);
+
+        JComboBox cStudentSex = new JComboBox();
+        cStudentSex.setModel(new DefaultComboBoxModel(new String[] {"\u7537", "\u5973"}));
+        cStudentSex.setBounds(202, 84, 106, 24);
+        contentPane.add(cStudentSex);
+
+        JFormattedTextField tStudentAge = new JFormattedTextField(NumberFormat.getIntegerInstance());
+        tStudentAge.setBounds(202, 115, 106, 24);
+        contentPane.add(tStudentAge);
+
+        JButton submintBtn = new JButton("\u6CE8\u518C");
+        submintBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
 
             }
         });
+        submintBtn.setBounds(161, 217, 113, 27);
+        contentPane.add(submintBtn);
     }
-
     public static void main(String[] args) {
-        new StudentRegisterFrame(400, 300).setVisible(true);
+        new StudentRegisterFrame(450, 300).setVisible(true);
     }
 }
