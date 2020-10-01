b7257781516987a312f3e97ef4a5fe768a57328b
chengiiii <1692485859@qq.com>
2020-10-02 01:03:52 +0800

    修复 未加项目登录失败

--- src/DAO/StudentDAO.java
+++ src/DAO/StudentDAO.java
@@ -19,7 +19,7 @@ public class StudentDAO implements Loginable {
         ArrayList<HashMap<String, String>> result = DBManager.query(String.format("SELECT * FROM Students WHERE student_id = %d AND student_password = '%s'", StudentID, StudentPassword));
         if (result.size() > 0) {
             String projectIdStr = result.get(0).get("project_id");
-            int projectId = projectIdStr.equals("null") ? 0 : Integer.parseInt(projectIdStr);
+            int projectId = projectIdStr == null ? 0 : Integer.parseInt(projectIdStr);
                     student = new StudentDTO(StudentID, result.get(0).get("student_name"), result.get(0).get("student_sex"),Integer.parseInt(result.get(0).get("student_age")), result.get(0).get("student_password"), projectId);
         }
         return student;
